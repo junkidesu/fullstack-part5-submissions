fae60b7b367707b9b467ee5f559d3fe87aecafda
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-12 21:37:44 +0500

    Blog posts have full and short forms

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [full, setFull] = useState(false)
+
+  const blogStyle = {
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderRadius: '5px',
+    paddingTop: '10px',
+    paddingLeft: '2px',
+    marginTop: '5px',
+    marginBottom: '5px'
+  }
+
+  const detailsStyle = {
+    display: full
+      ? ''
+      : 'none'
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setFull(!full)}>
+          {full ? 'hide' : 'view'}
+        </button>
+      </div>
+      <div style={detailsStyle}>
+        {blog.url} <br />
+        {blog.likes} <button>like</button> <br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
