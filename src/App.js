36de559c7588f1de5a3bcf1f0ba4397b48afab29
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-13 10:47:55 +0500

    Blogs are sorted by likes

--- src/App.js
+++ src/App.js
@@ -248,7 +248,9 @@ const App = () => {
         <CreateBlogForm createBlog={createBlog} />
       </Togglable>
 
-      {blogs.map(blog =>
+      {blogs
+        .sort((b1, b2) => b1.likes - b2.likes)
+        .map(blog =>
         <Blog
           key={blog.id}
           blog={blog}
