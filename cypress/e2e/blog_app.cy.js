3ea5365a50790f7a821d543287adbf087ca260aa
Anwar Sh <alvaro.sh03@gmail.com>
2022-10-05 02:26:49 +0500

    Refactor the tests

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,20 +1,28 @@
 describe('Blog App', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users/', {
+      username: 'someusername',
+      name: 'Some Full Name',
+      password: 'somepassword'
+    })
     cy.visit('http://localhost:3000')
   })
+
   it('Login form is shown', function() {
     cy.contains('log in to application')
   })
 
   describe('Login', function() {
     it('succeeds with correct credentials', function() {
-      cy.get('#username').type('username')
-      cy.get('#password').type('password')
+      cy.get('#username').type('someusername')
+      cy.get('#password').type('somepassword')
       cy.get('#login-button').click()
 
-      cy.contains('logged in as username')
+      cy.contains('logged in as someusername')
       cy.get('.error').should('have.css', 'color', 'rgb(0, 128, 0)')
     })
+
     it('fails with wrong credentials', function() {
       cy.get('#username').type('wrongusername')
       cy.get('#password').type('wrongpassword')
